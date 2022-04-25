eeec01609e27c8349dd555ada0cf399de5417c09
Burgos <98974574+nandobfer@users.noreply.github.com>
2022-04-25 19:52:27 -0300

    Started algorithym

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-with open("errors.txt", "a") as file_error:
-            file_error.write(file_name + '\n')
+with open("style.css", "r") as file:
+    file = file.read()
+
+def getLines(file):
+    lines = []
+    line = []
+    count = 0
+    for char in file:
+        line.append(char)
+        if char == '\n':
+            lines.append(''.join(line))
+            line = []
+            count += 1
+    return lines
+    
+def getWidths(lines):
+    for line in lines:
+        if 'width' in line:
+            print('width ', line[10])
+    
+lines = getLines(file)
+getWidths(lines)
